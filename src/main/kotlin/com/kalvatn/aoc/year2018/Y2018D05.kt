97e7eab18aa7c4b54aa503d20f0bcd9cb4ca1084
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2018-12-05 00:09:14 +0100

    prepare 2018 day 5

---
+++ src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D05.kt
@@ -0,0 +1,17 @@
+package com.kalvatn.aoc.year2018
+
+import com.kalvatn.aoc.common.Day
+import com.kalvatn.aoc.common.PuzzleInput
+
+
+class Y2018D05(input: PuzzleInput? = null) : APuzzle2018(Day.D05, input) {
+    override fun partOne(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun partTwo(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
---
+++ src/test/kotlin/com/kalvatn/aoc/year2018/Y2018D05Test.kt
@@ -0,0 +1,31 @@
+package com.kalvatn.aoc.year2018
+
+import com.kalvatn.aoc.IPuzzleTest
+import com.kalvatn.aoc.common.PuzzleInput
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert
+import org.junit.Test
+
+class Y2018D05Test : IPuzzleTest {
+    @Test
+    override fun examplePartOne() {
+        val test1 = Y2018D05(PuzzleInput.ofSingleLine(""))
+        Assert.assertThat(test1.partOne(), equalTo(""))
+    }
+
+    @Test
+    override fun examplePartTwo() {
+        val test1 = Y2018D05(PuzzleInput.ofSingleLine(""))
+        Assert.assertThat(test1.partTwo(), equalTo(""))
+    }
+
+    @Test
+    override fun solutionPartOne() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    @Test
+    override fun solutionPartTwo() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
