5235dd77750e11908de7daa5076bdac9647bd98f
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2018-12-06 19:19:35 +0100

    2018 day 5 cleanup

--- src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D05.kt
+++ src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D05.kt
@@ -7,32 +7,32 @@ import java.util.*
 
 class Y2018D05(input: PuzzleInput? = null) : APuzzle2018(Day.D05, input) {
 
-    private val polymer: String = this.input.singleLine()
-    private val aBBa: Set<Char> = polymer.map { it.toLowerCase() }.toSet()
 
     private fun Char.reacts(b: Char): Boolean {
         return this != b && this.equals(b, ignoreCase = true)
     }
 
-    private fun react(string: String): Int {
-        return string.fold(Stack<Char>()) { acc, c ->
+    private fun react(string: Iterable<Char>): Iterable<Char> {
+        return string.fold(Stack()) { acc, c ->
             if (!acc.isEmpty() && acc.last().reacts(c)) {
                 acc.pop()
             } else {
                 acc.push(c)
             }
-            return@fold acc
-        }.count()
+            acc
+        }
     }
 
+    private val polymer: String = this.input.singleLine()
+    private val part1: Iterable<Char> = react(polymer.asIterable())
 
     override fun partOne(): String {
-        return react(polymer).toString()
+        return react(polymer.asIterable()).count().toString()
     }
 
     override fun partTwo(): String {
-        return aBBa.map { it ->
-            react(polymer.filter { c -> !c.equals(it, ignoreCase = true) })
+        return ('a'..'z').map { it ->
+            react(part1.filter { c -> !c.equals(it, ignoreCase = true) }).count()
         }.min().toString()
     }
 }
