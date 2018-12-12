ad7d57864b4f582554e032d18b2288ff3bea4d11
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2018-12-12 17:27:47 +0100

    2018 day 12, messy

--- src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D12.kt
+++ src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D12.kt
@@ -2,14 +2,73 @@ package com.kalvatn.aoc.year2018
 
 import com.kalvatn.aoc.common.Day
 import com.kalvatn.aoc.common.PuzzleInput
+import com.kalvatn.aoc.common.Year
+import com.kalvatn.aoc.exceptions.Impossiburu
 
 class Y2018D12(input: PuzzleInput? = null) : APuzzle2018(Day.D12, input) {
+
+
+    private val initialState = this.input.lines.first().split(":")[1].trim()
+    private val rules = this.input.lines.drop(1).map { it.split("=>") }.map { it[0].trim() to (it[1].trim() == "#") }.toMap()
+
+
+    fun nextGeneration(current:MutableMap<Int, Boolean>):MutableMap<Int, Boolean> {
+        val indexes = current.keys.map { key -> key }
+        val nextPlantIndexes = mutableMapOf<Int, Boolean>()
+        indexes.forEach { index ->
+            var segment = ""
+            (index - 2..index + 2).forEach { it ->
+                nextPlantIndexes.putIfAbsent(it, false)
+                segment += if (current.getOrDefault(it, false)) '#' else '.'
+            }
+            nextPlantIndexes[index] = rules.getOrDefault(segment, false)
+        }
+        return nextPlantIndexes
+    }
+
     override fun partOne(): String {
-        return ""
+
+        var plantIndexes = mutableMapOf<Int, Boolean>()
+        initialState.forEachIndexed { index, c ->
+            plantIndexes[index] = (c == '#')
+        }
+        (0 - 2..initialState.length+2).forEach {
+            plantIndexes.putIfAbsent(it, false)
+        }
+
+        val maxGenerations = 20
+        for(generation in 1..maxGenerations) {
+            val nextPlantIndexes = nextGeneration(plantIndexes)
+            plantIndexes = nextPlantIndexes
+        }
+        return plantIndexes.filter { it.value }.keys.sum().toString()
     }
 
     override fun partTwo(): String {
-        return ""
+        var plantIndexes = mutableMapOf<Int, Boolean>()
+        initialState.forEachIndexed { index, c ->
+            plantIndexes[index] = (c == '#')
+        }
+        (0 - 2..initialState.length+2).forEach {
+            plantIndexes.putIfAbsent(it, false)
+        }
+
+        var lastSum = 0L
+        val sums = mutableMapOf<Long, Int>()
+        val maxGenerations = 50000000000
+        for(generation in 1..maxGenerations) {
+            val nextPlantIndexes = nextGeneration(plantIndexes)
+            plantIndexes = nextPlantIndexes
+            val sum = plantIndexes.filter { it.value }.keys.map { it.toLong() }.sum()
+            val diff = sum - lastSum
+
+            sums[diff] = sums.getOrDefault(diff, 0).inc()
+            if (sums.getOrDefault(diff, 0) > 2) {
+                return (sum + (maxGenerations - generation) * diff).toString()
+            }
+            lastSum = sum
+        }
+        throw Impossiburu()
     }
 }
 
--- src/test/kotlin/com/kalvatn/aoc/year2018/Y2018D12Test.kt
+++ src/test/kotlin/com/kalvatn/aoc/year2018/Y2018D12Test.kt
@@ -11,24 +11,22 @@ class Y2018D12Test : BaseDayTest() {
     override fun examplePartOne() {
         //val test1 = Y2018D12(PuzzleInput.ofSingleLine(""))
         val test1 = Y2018D12(PuzzleInput.forDay(Year.Y2018, Day.D12, "test"))
-        Assert.assertThat(test1.partOne().toInt(), equalTo(0))
+        Assert.assertThat(test1.partOne().toInt(), equalTo(325))
     }
 
     override fun examplePartTwo() {
         //val test1 = Y2018D12(PuzzleInput.ofSingleLine(""))
         val test1 = Y2018D12(PuzzleInput.forDay(Year.Y2018, Day.D12, "test"))
-        Assert.assertThat(test1.partOne().toInt(), equalTo(0))
+        Assert.assertThat(test1.partTwo().toLong(), equalTo(350000000080))
     }
 
     override fun solutionPartOne() {
-        TODO("not implemented")
         val day = Y2018D12()
-        Assert.assertThat(day.partOne().toInt(), equalTo(0))
+        Assert.assertThat(day.partOne().toInt(), equalTo(3798))
     }
 
     override fun solutionPartTwo() {
-        TODO("not implemented")
         val day = Y2018D12()
-        Assert.assertThat(day.partTwo().toInt(), equalTo(0))
+        Assert.assertThat(day.partTwo().toLong(), equalTo(3900000002212))
     }
 }
