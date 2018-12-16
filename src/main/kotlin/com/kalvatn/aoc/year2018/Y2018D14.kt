bbb08f95e55487fabaa72ba070c36c8709550df4
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2018-12-16 04:17:36 +0100

    2018 day 14

--- src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D14.kt
+++ src/main/kotlin/com/kalvatn/aoc/year2018/Y2018D14.kt
@@ -4,17 +4,44 @@ import com.kalvatn.aoc.common.Day
 import com.kalvatn.aoc.common.PuzzleInput
 
 class Y2018D14(input: PuzzleInput? = null) : APuzzle2018(Day.D14, input) {
+
+    private fun createRecipes(partTwo:Boolean): String {
+        val recipes = mutableListOf(3, 7)
+        var elf1 = 0
+        var elf2 = 1
+        val target = this.input.lines.first()
+        val targetLength = target.length
+        while (true) {
+            val score1 = recipes[elf1]
+            val score2 = recipes[elf2]
+            recipes.addAll("${score1 + score2}".map { "$it".toInt() })
+            elf1 = (elf1 + score1 + 1) % recipes.size
+            elf2 = (elf2 + score2 + 1) % recipes.size
+
+            if (partTwo) {
+                when (target) {
+                    recipes.takeLast(targetLength).joinToString("") -> return (recipes.size - targetLength).toString()
+                    recipes.takeLast(targetLength + 1).dropLast(1).joinToString("") -> return (recipes.size - (targetLength + 1)).toString()
+                }
+            } else {
+                if ((recipes.size - 10) >= target.toInt()) {
+                    return recipes.drop(target.toInt()).take(10).joinToString("")
+                }
+            }
+        }
+    }
+
     override fun partOne(): String {
-        return ""
+        return createRecipes(false)
     }
 
     override fun partTwo(): String {
-        return ""
+        return createRecipes(true)
     }
 }
 
 fun main(args: Array<String>) {
-//    val day = Y2018D14(PuzzleInput.forDay(Year.Y2018, Day.D14, "test"))
+//    val day = Y2018D14(PuzzleInput.ofSingleLine("580741"))
     val day = Y2018D14()
     day.run()
 }
--- src/test/kotlin/com/kalvatn/aoc/year2018/Y2018D14Test.kt
+++ src/test/kotlin/com/kalvatn/aoc/year2018/Y2018D14Test.kt
@@ -9,26 +9,22 @@ import org.junit.Assert
 
 class Y2018D14Test : BaseDayTest() {
     override fun examplePartOne() {
-        //val test1 = Y2018D14(PuzzleInput.ofSingleLine(""))
-        val test1 = Y2018D14(PuzzleInput.forDay(Year.Y2018, Day.D14, "test"))
-        Assert.assertThat(test1.partOne().toInt(), equalTo(0))
+        val test1 = Y2018D14(PuzzleInput.ofSingleLine("5"))
+        Assert.assertThat(test1.partOne(), equalTo("0124515891"))
     }
 
     override fun examplePartTwo() {
-        //val test1 = Y2018D14(PuzzleInput.ofSingleLine(""))
-        val test1 = Y2018D14(PuzzleInput.forDay(Year.Y2018, Day.D14, "test"))
-        Assert.assertThat(test1.partOne().toInt(), equalTo(0))
+        val test1 = Y2018D14(PuzzleInput.ofSingleLine("59414"))
+        Assert.assertThat(test1.partTwo().toInt(), equalTo(2018))
     }
 
     override fun solutionPartOne() {
-        TODO("not implemented")
         val day = Y2018D14()
-        Assert.assertThat(day.partOne().toInt(), equalTo(0))
+        Assert.assertThat(day.partOne(), equalTo("6910849249"))
     }
 
     override fun solutionPartTwo() {
-        TODO("not implemented")
         val day = Y2018D14()
-        Assert.assertThat(day.partTwo().toInt(), equalTo(0))
+        Assert.assertThat(day.partTwo().toInt(), equalTo(20330673))
     }
 }
