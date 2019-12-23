ede993fc93ba24800a6683d0af2d2b705e419acc
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2019-12-23 18:46:51 +0100

    2019-20 part2

--- src/main/kotlin/com/kalvatn/aoc/year2019/Y2019D20.kt
+++ src/main/kotlin/com/kalvatn/aoc/year2019/Y2019D20.kt
@@ -7,6 +7,7 @@ import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2019
 import com.kalvatn.aoc.core.runner.PuzzleRunner
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import java.util.*
 
@@ -24,7 +25,7 @@ class Y2019D20(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2019(Day.D2
         map
     }
 
-    data class Portal(val name: String, val entrance: Point)
+    data class Portal(val name: String, val entrance: Point, val outer: Boolean = false)
 
     fun portals(maze: Map<Point, Char>): Map<String, Set<Portal>> {
         val portals = mutableMapOf<String, MutableSet<Portal>>()
@@ -39,7 +40,8 @@ class Y2019D20(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2019(Day.D2
                 }
                 println("$k $v $name")
 
-                portals.computeIfAbsent(name) { mutableSetOf() }.add(Portal(name, it))
+                val outer = it.x == 2 || it.y == 2 || it.x == maxX - 2 || it.y == maxY - 2
+                portals.computeIfAbsent(name) { mutableSetOf() }.add(Portal(name, it, outer))
             }
         }
         return portals
@@ -54,7 +56,16 @@ class Y2019D20(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2019(Day.D2
         return entranceToExit.toMap()
     }
 
-    data class Node(val point: Point, val distance: Int = 0, val justExited: Boolean = false)
+    data class Node(val point: Point, val distance: Int = 0, val justExited: Boolean = false, val depth: Int = 0) : Comparable<Node> {
+
+        override fun compareTo(other: Node): Int {
+            val d = distance.compareTo(other.distance)
+            if (d == 0) {
+                return depth.compareTo(other.depth)
+            }
+            return d
+        }
+    }
 
     fun nextNodes(node: Node, maze: Map<Point, Char>, p2p: Map<Point, Point>): Set<Node> {
         if (node.point in p2p && !node.justExited) {
@@ -115,8 +126,84 @@ class Y2019D20(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2019(Day.D2
         return shortestPath(maze, p2p, start, target).toString()
     }
 
+    val maxX = maze.keys.maxBy { it.x }!!.x
+    val maxY = maze.keys.maxBy { it.y }!!.y
+
+    fun nextNodes2(node: Node, maze: Map<Point, Char>, p2p: Map<Point, Point>): Set<Node> {
+        if (node.point in p2p && !node.justExited) {
+            val outer = node.point.x == 2 || node.point.y == 2 || node.point.x == maxX - 2 || node.point.y == maxY - 2
+            if (outer && node.depth > 0) {
+                return setOf(
+                        Node(p2p[node.point]
+                                ?: error("impossiburu"), node.distance + 1, true, node.depth - 1)
+                )
+            } else if (!outer && node.depth <= 100) {
+                return setOf(
+                        Node(p2p[node.point]
+                                ?: error("impossiburu"), node.distance + 1, true, node.depth + 1)
+                )
+
+            }
+        }
+        return node.point.adj4().filter { it in maze.keys }.filter { maze[it] == '.' }.map {
+            val np = it
+            Node(np, node.distance + 1, false, node.depth)
+        }.toSet()
+    }
+
+    suspend fun shortestPath2(maze: Map<Point, Char>, p2p: Map<Point, Point>, start: Node, target: Node): Int {
+
+        var best = Integer.MAX_VALUE
+
+        val queue = PriorityQueue<Node>()
+        queue.add(start)
+        val visited = mutableSetOf<Pair<Point, Int>>()
+
+        var steps = 0L
+        while (queue.isNotEmpty()) {
+            val current = queue.remove()
+
+            val hash = Pair(current.point, current.depth)
+            if (hash in visited) {
+                continue
+            }
+            visited.add(hash)
+
+//            if (steps++ % 10000L == 0L) {
+//                println(current)
+//            }
+
+            if (current.point == target.point && current.depth == 0) {
+                if (current.distance < best) {
+                    best = current.distance
+                    return best
+                }
+            }
+
+
+            nextNodes2(current, maze, p2p).forEach {
+                queue.add(it)
+            }
+        }
+
+        return best
+    }
+
     override suspend fun partTwo(): String {
-        return ""
+        val maze = maze.toMap()
+        maze.print {
+            print(maze[it])
+        }
+        println("max x/y = $maxX/$maxY")
+        val portals = portals(maze)
+        portals.forEach {
+            println(it)
+        }
+        val p2p = portalPoints(portals)
+        val start = Node((portals["AA"] ?: error("impossiburu")).first().entrance)
+        val target = Node((portals["ZZ"] ?: error("impossiburu")).first().entrance)
+
+        return shortestPath2(maze, p2p, start, target).toString()
     }
 
 }
--- src/test/kotlin/com/kalvatn/aoc/year2019/Y2019D20Test.kt
+++ src/test/kotlin/com/kalvatn/aoc/year2019/Y2019D20Test.kt
@@ -17,18 +17,17 @@ internal class Y2019D20Test : BaseDayTest() {
 
     @Test
     override suspend fun examplePartTwo() {
-//        val input = PuzzleInput.ofSingleLine("")
-//        val input = PuzzleInput.forDay(Year.Y2019, Day.D20, "test")
-//        Y2019D20(input).partTwo().toInt() shouldBe 0
+        val input3 = PuzzleInput.forDay(Year.Y2019, Day.D20, "test3")
+        Y2019D20(input3).partTwo().toInt() shouldBe 396
     }
 
     @Test
     override suspend fun solutionPartOne() {
-//        Y2019D20().partOne().toInt() shouldBe 0
+        Y2019D20().partOne().toInt() shouldBe 690
     }
 
     @Test
     override suspend fun solutionPartTwo() {
-//        Y2019D20().partTwo().toInt() shouldBe 0
+        Y2019D20().partTwo().toInt() shouldBe 7976
     }
 }
