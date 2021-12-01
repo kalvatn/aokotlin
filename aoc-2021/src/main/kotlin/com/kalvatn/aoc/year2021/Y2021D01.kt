43a230313242bd3d692d0696c46cd116faa6d056
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-01 09:37:50 +0100

    2021-01 part1

---
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D01.kt
@@ -0,0 +1,39 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.GenericPuzzle2021
+import com.kalvatn.aoc.core.model.Year
+import com.kalvatn.aoc.core.runner.PuzzleRunner
+import kotlinx.coroutines.runBlocking
+
+class Y2021D01(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D01, input) {
+
+  private val lines by lazy { this.input.lines }
+
+  override suspend fun partOne(): String {
+    var count = 0
+    val measurements = lines.map { it.toInt() }
+    var prev = measurements.first()
+    measurements.drop(1).forEach { measurement ->
+      if (measurement > prev) {
+        count = count.inc()
+      }
+      prev = measurement
+    }
+    return "$count"
+  }
+
+  override suspend fun partTwo(): String {
+    return ""
+  }
+
+  companion object {
+  }
+}
+
+fun main() = runBlocking {
+  val input = PuzzleInput.p1Test(Year.Y2021, Day.D01)
+  PuzzleRunner.run(Y2021D01(input))
+  PuzzleRunner.run(Y2021D01())
+}
---
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D01Test.kt
@@ -0,0 +1,32 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.BaseDayTest
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.Year
+import io.kotest.matchers.shouldBe
+
+internal class Y2021D01Test : BaseDayTest() {
+
+  @Test
+  override suspend fun examplePartOne() {
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D01)
+    Y2021D01(input).partOne() shouldBe "7"
+  }
+
+  @Test
+  override suspend fun examplePartTwo() {
+    val input = PuzzleInput.p2Test(Year.Y2021, Day.D01)
+    Y2021D01(input).partTwo() shouldBe ""
+  }
+
+  @Test
+  override suspend fun solutionPartOne() {
+    Y2021D01().partOne() shouldBe "1233"
+  }
+
+  @Test
+  override suspend fun solutionPartTwo() {
+    Y2021D01().partTwo() shouldBe ""
+  }
+}
