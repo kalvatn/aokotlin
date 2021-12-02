b2805d5995216b58c5fd299aa55e81df18cefeee
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-02 06:26:29 +0100

    aoc-2021-02

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D02.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D02.kt
@@ -1,33 +1,56 @@
 package com.kalvatn.aoc.year2021
 
+import com.kalvatn.aoc.common.model.Point
 import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2021
-import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
+import com.kalvatn.aoc.exceptions.Impossiburu
 import kotlinx.coroutines.runBlocking
 
 class Y2021D02(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D02, input) {
 
-  private val lines by lazy { this.input.lines }
+  private val instructions by lazy {
+    this.input.lines.map {
+      val (dir, count) = it.split(" ")
+      Pair(dir, count.toInt())
+    }
+  }
 
   override suspend fun partOne(): String {
-    lines.forEach {
-      println(it)
+    val pos = instructions.fold(Point(0, 0)) { acc, cmd ->
+      val (dir, count) = cmd
+      when (dir) {
+        "forward" -> acc.copy(x = acc.x + count)
+        "up" -> acc.copy(y = acc.y - count)
+        "down" -> acc.copy(y = acc.y + count)
+        else -> throw Impossiburu()
+      }
     }
-    return ""
+    return (pos.x * pos.y).toString()
   }
 
   override suspend fun partTwo(): String {
-    return ""
-  }
-
-  companion object {
+    val (pos, _) = instructions.fold(Pair(Point(0, 0), 0)) { acc, cmd ->
+      val (pos, aim) = acc
+      val (dir, count) = cmd
+      when (dir) {
+        "forward" -> {
+          val updatedPos = pos.copy(
+            x = pos.x + (count),
+            y = pos.y + (count * aim)
+          )
+          Pair(updatedPos, aim)
+        }
+        "up" -> Pair(pos, aim - count)
+        "down" -> Pair(pos, aim + count)
+        else -> throw Impossiburu()
+      }
+    }
+    return (pos.x * pos.y).toString()
   }
 }
 
 fun main() = runBlocking {
-  val input = PuzzleInput.p1Test(Year.Y2021, Day.D02)
-  PuzzleRunner.run(Y2021D02(input))
   PuzzleRunner.run(Y2021D02())
 }
--- aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D02Test.kt
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D02Test.kt
@@ -11,22 +11,22 @@ internal class Y2021D02Test : BaseDayTest() {
   @Test
   override suspend fun examplePartOne() {
     val input = PuzzleInput.p1Test(Year.Y2021, Day.D02)
-    Y2021D02(input).partOne() shouldBe ""
+    Y2021D02(input).partOne() shouldBe "150"
   }
 
   @Test
   override suspend fun examplePartTwo() {
-    val input = PuzzleInput.p2Test(Year.Y2021, Day.D02)
-    Y2021D02(input).partTwo() shouldBe ""
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D02)
+    Y2021D02(input).partTwo() shouldBe "900"
   }
 
   @Test
   override suspend fun solutionPartOne() {
-    Y2021D02().partOne() shouldBe ""
+    Y2021D02().partOne() shouldBe "2150351"
   }
 
   @Test
   override suspend fun solutionPartTwo() {
-    Y2021D02().partTwo() shouldBe ""
+    Y2021D02().partTwo() shouldBe "1842742223"
   }
 }
