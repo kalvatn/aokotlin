32243f4556bdb3b62093202dc9fa629f3ae11a6a
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-02 23:29:50 +0100

    aoc-2021-03 prep

---
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D03Test.kt
@@ -0,0 +1,32 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.BaseDayTest
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.Year
+import io.kotest.matchers.shouldBe
+
+internal class Y2021D03Test : BaseDayTest() {
+
+  @Test
+  override suspend fun examplePartOne() {
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D03)
+    Y2021D03(input).partOne() shouldBe ""
+  }
+
+  @Test
+  override suspend fun examplePartTwo() {
+    val input = PuzzleInput.p2Test(Year.Y2021, Day.D03)
+    Y2021D03(input).partTwo() shouldBe ""
+  }
+
+  @Test
+  override suspend fun solutionPartOne() {
+    Y2021D03().partOne() shouldBe ""
+  }
+
+  @Test
+  override suspend fun solutionPartTwo() {
+    Y2021D03().partTwo() shouldBe ""
+  }
+}
