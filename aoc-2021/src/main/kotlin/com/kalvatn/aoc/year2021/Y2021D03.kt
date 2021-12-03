67deb9f544a1e45f113daf34118200974d8ac677
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-03 15:50:36 +0100

    2021-03 refactor

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D03.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D03.kt
@@ -3,81 +3,55 @@ package com.kalvatn.aoc.year2021
 import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2021
-import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
 import kotlinx.coroutines.runBlocking
 
 class Y2021D03(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D03, input) {
 
+  companion object {
+    const val BIT_ONE = '1'
+    const val BIT_ZERO = '0'
+  }
+
   private val lines by lazy { this.input.lines }
+  private val indices = lines.first().indices
+
+  private fun List<String>.countMatchingAtIndex(c: Char, index: Int) =
+    this.map { it[index] }.count { it == c }
+
+  private fun List<String>.countOnesAndZerosAtIndex(index: Int) =
+    Pair(
+      countMatchingAtIndex(BIT_ZERO, index),
+      countMatchingAtIndex(BIT_ONE, index)
+    )
 
   override suspend fun partOne(): String {
-    val indexToBit = mutableMapOf<Int, MutableList<Int>>()
-    lines.flatMap { number ->
-      number.mapIndexed { index, c -> index to c }
-    }.map {
-      indexToBit.computeIfAbsent(it.first) { mutableListOf() }.add(it.second.digitToInt())
-    }
-    var gamma = ""
-    var epsilon = ""
-    indexToBit.forEach { (_, v) ->
-      if (v.count { it == 0 } > v.count { it == 1 }) {
-        gamma += "0"
-        epsilon += "1"
-      } else {
-        gamma += "1"
-        epsilon += "0"
+    val gamma = indices.fold("") { acc, index ->
+      val (count0, count1) = lines.countOnesAndZerosAtIndex(index)
+      acc + if (count0 > count1) BIT_ZERO else BIT_ONE
+    }.toInt(2)
+    val epsilon = gamma.xor((1 shl indices.last.inc()) - 1)
+    return (gamma * epsilon).toString()
+  }
+
+  private fun filter(numbers: List<String>, oneBitDecider: (Int, Int) -> Boolean): Int {
+    return indices.fold(numbers) { acc, index ->
+      val (count0, count1) = acc.countOnesAndZerosAtIndex(index)
+      when {
+        acc.size == 1 -> acc
+        oneBitDecider(count0, count1) -> acc.filter { it[index] == BIT_ONE }
+        else -> acc.filter { it[index] == BIT_ZERO }
       }
-    }
-    val a = gamma.toInt(2)
-    val b = epsilon.toInt(2)
-    return (a * b).toString()
+    }.first().toInt(2)
   }
 
   override suspend fun partTwo(): String {
-    var i = 0
-    val linesRemaining = lines.toMutableList()
-    while (linesRemaining.size > 1) {
-      val count0 = linesRemaining.map { it[i] }.count { it == '0' }
-      val count1 = linesRemaining.map { it[i] }.count { it == '1' }
-      if (count1 >= count0) {
-        linesRemaining.removeAll { it[i] != '1' }
-      } else {
-        linesRemaining.removeAll { it[i] != '0' }
-      }
-      i++
-    }
-    val oxygen = linesRemaining.first().toInt(2)
-
-    val linesRemaining2 = lines.toMutableList()
-    i = 0
-    while (linesRemaining2.size > 1) {
-      val count0 = linesRemaining2.map { it[i] }.count { it == '0' }
-      val count1 = linesRemaining2.map { it[i] }.count { it == '1' }
-      println(linesRemaining2)
-      println("$i $count1 $count0")
-      if (count0 <= count1) {
-        linesRemaining2.removeAll {
-          it[i] != '0'
-        }
-      } else {
-        linesRemaining2.removeAll {
-          it[i] != '1'
-        }
-      }
-      i++
-    }
-    val co2 = linesRemaining2.first().toInt(2)
-
-    println("oxygen $oxygen")
-    println("co2 $co2")
-
+    val oxygen = filter(lines) { count0, count1 -> count1 >= count0 }
+    val co2 = filter(lines) { count0, count1 -> count1 < count0 }
     return (oxygen * co2).toString()
   }
 }
 
 fun main() = runBlocking {
-  val input = PuzzleInput.p1Test(Year.Y2021, Day.D03)
-  PuzzleRunner.run(Y2021D03(input))
   PuzzleRunner.run(Y2021D03())
 }
