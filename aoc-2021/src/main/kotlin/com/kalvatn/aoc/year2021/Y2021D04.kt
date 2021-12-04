17b5d68063ec1815f9d5047d1f49e4d071252f6c
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-04 14:23:10 +0100

    aoc-2021-04 refactor

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D04.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D04.kt
@@ -3,114 +3,85 @@ package com.kalvatn.aoc.year2021
 import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2021
-import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
+import com.kalvatn.aoc.exceptions.Impossiburu
 import kotlinx.coroutines.runBlocking
 
 class Y2021D04(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D04, input) {
 
+  companion object {
+    const val BOARD_SIZE = 5
+  }
+
   private val lines by lazy { this.input.lines }
 
-  data class Board(val board: List<MutableList<Int>>) {
-    val checked: List<MutableList<Boolean>> = board.map { it.map { false }.toMutableList() }
-    fun setChecked(row: Int, col: Int) {
-      checked[row][col] = true
-    }
+  data class BingoBoard(val numbers: List<MutableList<Int>>) {
+    private val checked: List<MutableList<Boolean>> = numbers.map { it.map { false }.toMutableList() }
+    private val indices = numbers.indices
+    private var hasBingo = false
 
-    fun checkValue(value: Int) {
-      board.forEachIndexed { y, row ->
-        row.forEachIndexed { x, col ->
-          if (col == value) {
-            setChecked(y, x)
+    fun checkNumber(number: Int) {
+      (indices).forEach { y ->
+        (indices).forEach { x ->
+          if (numbers[y][x] == number) {
+            checked[y][x] = true
           }
         }
       }
     }
 
-    fun sumAllUnchecked(): Int {
-      return checked.flatMapIndexed { y, row ->
-        row.mapIndexed { x, col ->
-          if (!col) {
-            board[y][x]
-          } else {
-            0
-          }
+    fun sumAllUnchecked(): Int =
+      (indices).flatMap { y ->
+        (indices).map { x ->
+          if (!checked[y][x]) numbers[y][x] else 0
         }
       }.sum()
-    }
 
-    fun has5(): Boolean {
-      val horizontal = checked.map { row -> row.all { it } }.any { it }
-      val vertical = (board.indices).map { col ->
-        checked.map {
-          it[col]
-        }.all { it }
+    private fun hasFilledHorizontal() =
+      checked.map { row -> row.all { it } }.any { it }
+
+    private fun hasFilledVertical() =
+      (numbers.indices).map { x ->
+        checked.map { it[x] }.all { it }
       }.any { it }
-      return horizontal || vertical
+
+    fun hasBingo(): Boolean {
+      hasBingo = hasBingo || hasFilledHorizontal() || hasFilledVertical()
+      return hasBingo
     }
   }
 
-  override suspend fun partOne(): String {
-    val draws = lines.first().split(",").map { it.toInt() }
-    println(draws)
-    val boards = lines.drop(1).windowed(5, 5).map { board ->
-      Board(board.map { s -> s.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toMutableList() })
-    }
-    draws.forEach { number ->
-      boards.forEach { board ->
-        board.checkValue(number)
+  private val numbers = lines.first().split(",").map { it.toInt() }
+  private val boards = lines.drop(1).windowed(BOARD_SIZE, BOARD_SIZE).map { board ->
+    BingoBoard(board.map { s -> s.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toMutableList() })
+  }
 
-        if (board.has5()) {
-          board.checked.forEach {
-            println(it)
-          }
-          println(board.sumAllUnchecked())
-          println(board.sumAllUnchecked() * number)
+  override suspend fun partOne(): String {
+    numbers.forEach { number ->
+      boards.forEach { board ->
+        board.checkNumber(number)
+        if (board.hasBingo()) {
           return (board.sumAllUnchecked() * number).toString()
         }
       }
     }
-    return ""
+    throw Impossiburu()
   }
 
   override suspend fun partTwo(): String {
-    val draws = lines.first().split(",").map { it.toInt() }
-    println(draws)
-    val boards = lines.drop(1).windowed(5, 5).map { board ->
-      Board(board.map { s -> s.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toMutableList() })
-    }
-
-    val boardsWon = boards.indices.associateWith { false }.toMutableMap()
-    var last = ""
-    draws.forEach { number ->
-      boards.forEachIndexed { index, board ->
-        if (!boardsWon.getValue(index)) {
-          board.checkValue(number)
-
-          if (board.has5()) {
-            boardsWon[index] = true
-            board.checked.forEach {
-              println(it)
-            }
-            board.board.forEach {
-              println(it)
-            }
-            val sumAllUnchecked = board.sumAllUnchecked()
-            last = (sumAllUnchecked * number).toString()
-            println("$index $sumAllUnchecked $number $last")
-          }
+    numbers.forEach { number ->
+      boards.filterNot { it.hasBingo() }.forEach { board ->
+        board.checkNumber(number)
+        if (board.hasBingo() && boards.all { it.hasBingo() }) {
+          val sumAllUnchecked = board.sumAllUnchecked()
+          return (sumAllUnchecked * number).toString()
         }
       }
     }
-    return last
-  }
-
-  companion object {
+    throw Impossiburu()
   }
 }
 
 fun main() = runBlocking {
-  val input = PuzzleInput.p1Test(Year.Y2021, Day.D04)
-  PuzzleRunner.run(Y2021D04(input))
   PuzzleRunner.run(Y2021D04())
 }
