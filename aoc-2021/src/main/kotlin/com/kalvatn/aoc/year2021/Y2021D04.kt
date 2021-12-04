2296c852f237d879af05e366f96143f451f5ec76
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-04 13:33:42 +0100

    aoc-2021-04 part2

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D04.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D04.kt
@@ -6,18 +6,18 @@ import com.kalvatn.aoc.core.model.GenericPuzzle2021
 import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
 import kotlinx.coroutines.runBlocking
-import kotlin.system.exitProcess
 
 class Y2021D04(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D04, input) {
 
   private val lines by lazy { this.input.lines }
 
-  data class Board(val board:List<MutableList<Int>>) {
-    val checked:List<MutableList<Boolean>> = board.map { it.map { false }.toMutableList() }
-    fun setChecked(row:Int, col:Int) {
+  data class Board(val board: List<MutableList<Int>>) {
+    val checked: List<MutableList<Boolean>> = board.map { it.map { false }.toMutableList() }
+    fun setChecked(row: Int, col: Int) {
       checked[row][col] = true
     }
-    fun checkValue(value:Int) {
+
+    fun checkValue(value: Int) {
       board.forEachIndexed { y, row ->
         row.forEachIndexed { x, col ->
           if (col == value) {
@@ -26,20 +26,24 @@ class Y2021D04(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D0
         }
       }
     }
+
     fun sumAllUnchecked(): Int {
       return checked.flatMapIndexed { y, row ->
         row.mapIndexed { x, col ->
-          if (!col) { board[y][x] } else { 0 }
+          if (!col) {
+            board[y][x]
+          } else {
+            0
+          }
         }
       }.sum()
     }
+
     fun has5(): Boolean {
-      val horizontal = checked.map { row ->
-        row.all { it }
-      }.any { it }
-      val vertical = checked.map { row ->
-        (board.indices).map { col ->
-          row[col]
+      val horizontal = checked.map { row -> row.all { it } }.any { it }
+      val vertical = (board.indices).map { col ->
+        checked.map {
+          it[col]
         }.all { it }
       }.any { it }
       return horizontal || vertical
@@ -70,7 +74,35 @@ class Y2021D04(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D0
   }
 
   override suspend fun partTwo(): String {
-    return ""
+    val draws = lines.first().split(",").map { it.toInt() }
+    println(draws)
+    val boards = lines.drop(1).windowed(5, 5).map { board ->
+      Board(board.map { s -> s.split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toMutableList() })
+    }
+
+    val boardsWon = boards.indices.associateWith { false }.toMutableMap()
+    var last = ""
+    draws.forEach { number ->
+      boards.forEachIndexed { index, board ->
+        if (!boardsWon.getValue(index)) {
+          board.checkValue(number)
+
+          if (board.has5()) {
+            boardsWon[index] = true
+            board.checked.forEach {
+              println(it)
+            }
+            board.board.forEach {
+              println(it)
+            }
+            val sumAllUnchecked = board.sumAllUnchecked()
+            last = (sumAllUnchecked * number).toString()
+            println("$index $sumAllUnchecked $number $last")
+          }
+        }
+      }
+    }
+    return last
   }
 
   companion object {
--- aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D04Test.kt
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D04Test.kt
@@ -16,8 +16,8 @@ internal class Y2021D04Test : BaseDayTest() {
 
   @Test
   override suspend fun examplePartTwo() {
-    val input = PuzzleInput.p2Test(Year.Y2021, Day.D04)
-    Y2021D04(input).partTwo() shouldBe ""
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D04)
+    Y2021D04(input).partTwo() shouldBe "1924"
   }
 
   @Test
@@ -27,6 +27,6 @@ internal class Y2021D04Test : BaseDayTest() {
 
   @Test
   override suspend fun solutionPartTwo() {
-    Y2021D04().partTwo() shouldBe ""
+    Y2021D04().partTwo() shouldBe "24742"
   }
 }
