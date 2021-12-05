164013fb03e2eee0c1d935b053da15ff9da17df1
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-05 13:38:43 +0100

    aoc-2021-05

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D05.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D05.kt
@@ -1,29 +1,69 @@
 package com.kalvatn.aoc.year2021
 
+import com.kalvatn.aoc.common.model.Point
 import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2021
 import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
 import kotlinx.coroutines.runBlocking
+import java.lang.Integer.max
+import kotlin.math.abs
+import kotlin.math.min
+import kotlin.math.sign
 
 class Y2021D05(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D05, input) {
 
   private val lines by lazy { this.input.lines }
 
+  private val re = "(\\d+),(\\d+) -> (\\d+),(\\d+)".toRegex()
+
+  data class Pipeline(val start: Point, val end: Point)
+
   override suspend fun partOne(): String {
-    lines.forEach {
-      println(it)
+    val pipelines = lines.map { line ->
+      val (x1, y1, x2, y2) = re.matchEntire(line)!!.destructured.toList().map { it.toInt() }
+      Pipeline(Point(x1, y1), Point(x2, y2))
     }
-    return ""
+    val horizontalOrVertical = pipelines.filter { it.start.x == it.end.x || it.start.y == it.end.y }
+    val total = mutableMapOf<Point, Int>()
+    horizontalOrVertical.forEach {
+      val x1 = min(it.start.x, it.end.x)
+      val x2 = max(it.start.x, it.end.x)
+      val y1 = min(it.start.y, it.end.y)
+      val y2 = max(it.start.y, it.end.y)
+      (x1..x2).map { x ->
+        (y1..y2).map { y ->
+          val point = Point(x, y)
+          total.computeIfAbsent(point) { 0 }
+          total[point] = total[point]!!.inc()
+        }
+      }
+    }
+    println(total)
+    return total.count { it.value >= 2 }.toString()
   }
 
   override suspend fun partTwo(): String {
-    return ""
+    val pipelines = lines.map { line ->
+      val (x1, y1, x2, y2) = re.matchEntire(line)!!.destructured.toList().map { it.toInt() }
+      Pipeline(Point(x1, y1), Point(x2, y2))
+    }
+    val points = pipelines
+      .flatMap {
+        val diff = it.end - it.start
+        val max = max(abs(diff.x), abs(diff.y))
+        val direction = Point(diff.x.sign, diff.y.sign)
+        (0..max).map { i ->
+          it.start + direction * i
+        }
+      }
+      .groupingBy { it }
+      .eachCount()
+      .count { it.value >= 2 }
+    return points.toString()
   }
 
-  companion object {
-  }
 }
 
 fun main() = runBlocking {
--- aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D05Test.kt
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D05Test.kt
@@ -11,22 +11,22 @@ internal class Y2021D05Test : BaseDayTest() {
   @Test
   override suspend fun examplePartOne() {
     val input = PuzzleInput.p1Test(Year.Y2021, Day.D05)
-    Y2021D05(input).partOne() shouldBe ""
+    Y2021D05(input).partOne() shouldBe "5"
   }
 
   @Test
   override suspend fun examplePartTwo() {
-    val input = PuzzleInput.p2Test(Year.Y2021, Day.D05)
-    Y2021D05(input).partTwo() shouldBe ""
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D05)
+    Y2021D05(input).partTwo() shouldBe "12"
   }
 
   @Test
   override suspend fun solutionPartOne() {
-    Y2021D05().partOne() shouldBe ""
+    Y2021D05().partOne() shouldBe "4745"
   }
 
   @Test
   override suspend fun solutionPartTwo() {
-    Y2021D05().partTwo() shouldBe ""
+    Y2021D05().partTwo() shouldBe "18442"
   }
 }
