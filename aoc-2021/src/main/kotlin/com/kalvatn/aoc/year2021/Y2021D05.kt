91bab2b6155c997ca3c81f2bd98f4e7f07c49d3b
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-05 13:08:19 +0100

    aoc-2021-05 prep

---
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D05.kt
@@ -0,0 +1,33 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.GenericPuzzle2021
+import com.kalvatn.aoc.core.model.Year
+import com.kalvatn.aoc.core.runner.PuzzleRunner
+import kotlinx.coroutines.runBlocking
+
+class Y2021D05(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D05, input) {
+
+  private val lines by lazy { this.input.lines }
+
+  override suspend fun partOne(): String {
+    lines.forEach {
+      println(it)
+    }
+    return ""
+  }
+
+  override suspend fun partTwo(): String {
+    return ""
+  }
+
+  companion object {
+  }
+}
+
+fun main() = runBlocking {
+  val input = PuzzleInput.p1Test(Year.Y2021, Day.D05)
+  PuzzleRunner.run(Y2021D05(input))
+  PuzzleRunner.run(Y2021D05())
+}
