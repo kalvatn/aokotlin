503e97666d9e23d3f75fbae27de89ff859fbe343
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-06 17:02:41 +0100

    aoc-2021-06

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D06.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D06.kt
@@ -3,31 +3,38 @@ package com.kalvatn.aoc.year2021
 import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2021
-import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
 import kotlinx.coroutines.runBlocking
 
 class Y2021D06(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D06, input) {
 
   private val lines by lazy { this.input.lines }
+  private val fish = lines.first().split(",").map { it.toInt() }
+
+  private val spawnCountCache = mutableMapOf<Int, Long>()
+
+  private fun spawnCount(daysLeft: Int): Long {
+    if (spawnCountCache.containsKey(daysLeft)) {
+      return spawnCountCache.getValue(daysLeft)
+    }
+    if (daysLeft <= 0) return 1
+
+    val c1 = spawnCount(daysLeft - 9)
+    val c2 = spawnCount(daysLeft - 7)
+    val sum = c1 + c2
+    spawnCountCache[daysLeft] = sum
+    return c1 + c2
+  }
 
   override suspend fun partOne(): String {
-    lines.forEach {
-      println(it)
-    }
-    return ""
+    return fish.sumOf { spawnCount(80 - it) }.toString()
   }
 
   override suspend fun partTwo(): String {
-    return ""
-  }
-
-  companion object {
+    return fish.sumOf { spawnCount(256 - it) }.toString()
   }
 }
 
 fun main() = runBlocking {
-  val input = PuzzleInput.p1Test(Year.Y2021, Day.D06)
-  PuzzleRunner.run(Y2021D06(input))
   PuzzleRunner.run(Y2021D06())
 }
--- aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D06Test.kt
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D06Test.kt
@@ -11,22 +11,22 @@ internal class Y2021D06Test : BaseDayTest() {
   @Test
   override suspend fun examplePartOne() {
     val input = PuzzleInput.p1Test(Year.Y2021, Day.D06)
-    Y2021D06(input).partOne() shouldBe ""
+    Y2021D06(input).partOne() shouldBe "5934"
   }
 
   @Test
   override suspend fun examplePartTwo() {
-    val input = PuzzleInput.p2Test(Year.Y2021, Day.D06)
-    Y2021D06(input).partTwo() shouldBe ""
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D06)
+    Y2021D06(input).partTwo() shouldBe "26984457539"
   }
 
   @Test
   override suspend fun solutionPartOne() {
-    Y2021D06().partOne() shouldBe ""
+    Y2021D06().partOne() shouldBe "355386"
   }
 
   @Test
   override suspend fun solutionPartTwo() {
-    Y2021D06().partTwo() shouldBe ""
+    Y2021D06().partTwo() shouldBe "1613415325809"
   }
 }
