78904a058569e9d29fe1764be042e75624bc9c5d
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-10 03:24:44 +0100

    aoc-2021-09 part1

---
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D09.kt
@@ -0,0 +1,56 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.common.model.Point
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.GenericPuzzle2021
+import com.kalvatn.aoc.core.model.Year
+import com.kalvatn.aoc.core.runner.PuzzleRunner
+import kotlinx.coroutines.runBlocking
+
+class Y2021D09(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D09, input) {
+
+  private val lines by lazy { this.input.lines }
+
+  override suspend fun partOne(): String {
+    val grid = lines.map { line -> line.map { it.digitToInt() } }
+    val risk = (0 until grid.size).flatMap { y ->
+      (0 until grid.first().size).map { x ->
+        val height = grid[y][x]
+        val c = Point(x, y)
+        val adj = c.adj4().mapNotNull {
+          try {
+            grid[it.y][it.x]
+          } catch (e: Exception) {
+            null
+          }
+        }
+        if (adj.all { it > height }) {
+          height.inc()
+        } else {
+          null
+        }
+      }
+    }
+      .filterNotNull()
+      .filter { it > 0 }
+      .also {
+        println(it)
+      }.sumOf { it }
+
+    return "$risk"
+  }
+
+  override suspend fun partTwo(): String {
+    return ""
+  }
+
+  companion object {
+  }
+}
+
+fun main() = runBlocking {
+  val input = PuzzleInput.p1Test(Year.Y2021, Day.D09)
+  PuzzleRunner.run(Y2021D09(input))
+  PuzzleRunner.run(Y2021D09())
+}
---
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D09Test.kt
@@ -0,0 +1,32 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.BaseDayTest
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.Year
+import io.kotest.matchers.shouldBe
+
+internal class Y2021D09Test : BaseDayTest() {
+
+  @Test
+  override suspend fun examplePartOne() {
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D09)
+    Y2021D09(input).partOne() shouldBe "15"
+  }
+
+  @Test
+  override suspend fun examplePartTwo() {
+    val input = PuzzleInput.p2Test(Year.Y2021, Day.D09)
+    Y2021D09(input).partTwo() shouldBe ""
+  }
+
+  @Test
+  override suspend fun solutionPartOne() {
+    Y2021D09().partOne() shouldBe "545"
+  }
+
+  @Test
+  override suspend fun solutionPartTwo() {
+    Y2021D09().partTwo() shouldBe ""
+  }
+}
