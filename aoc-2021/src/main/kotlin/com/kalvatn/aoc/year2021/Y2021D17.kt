1510f2060e941392063d856caf82251f96372beb
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-17 07:07:55 +0100

    aoc-2021-17 part1

--- aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D17.kt
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D17.kt
@@ -1,21 +1,79 @@
 package com.kalvatn.aoc.year2021
 
+import com.kalvatn.aoc.common.model.Point
 import com.kalvatn.aoc.core.input.PuzzleInput
 import com.kalvatn.aoc.core.model.Day
 import com.kalvatn.aoc.core.model.GenericPuzzle2021
 import com.kalvatn.aoc.core.model.Year
 import com.kalvatn.aoc.core.runner.PuzzleRunner
+import com.kalvatn.aoc.exceptions.Impossiburu
 import kotlinx.coroutines.runBlocking
 
 class Y2021D17(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D17, input) {
 
   private val lines by lazy { this.input.lines }
 
-  override suspend fun partOne(): String {
-    lines.forEach {
-      println(it)
+  fun newPosVel(pos: Point, velocity: Point): Pair<Point, Point> {
+    val newX = when {
+      velocity.x > 0 -> velocity.x.dec()
+      velocity.x < 0 -> velocity.x.inc()
+      else -> 0
     }
-    return ""
+    return Pair(
+      Point(pos.x + velocity.x, pos.y + velocity.y),
+      Point(newX, velocity.y.dec())
+    )
+  }
+
+  fun eval(initialVelocity: Point, targetX: IntRange, targetY: IntRange): Set<Point> {
+    var pos = Point(0, 0)
+    var vel = initialVelocity
+
+    val all = mutableListOf<Point>()
+
+    var within = false
+    var step = 0
+
+    while (step < 1000) {
+      val new = newPosVel(pos, vel)
+      pos = new.first
+      vel = new.second
+      if (within && (pos.x !in targetX || pos.y !in targetY)) {
+        break
+      }
+      all.add(pos)
+      if (pos.x in targetX && pos.y in targetY) {
+        within = true
+      }
+      step++
+    }
+    return all.dropLastWhile { it.x !in targetX || it.y !in targetY }.toSet()
+  }
+
+  override suspend fun partOne(): String {
+//    target area: x=20..30, y=-10..-5
+//    target area: x=119..176, y=-141..-84
+
+//    val targetX = (20..30)
+//    val targetY = (-10..-5)
+    val targetX = (119..176)
+    val targetY = (-141..-84)
+
+    val velocities = (-1000..1000).flatMap { velX ->
+      (-1000..1000).map { velY ->
+        Point(velX, velY)
+      }
+    }.toSet()
+//    val velocities = setOf(Point(7,2))
+//    val velocities = setOf(Point(6,9))
+//    val velocities = setOf(Point(17,-4))
+
+    val max = velocities.mapNotNull { velocity ->
+      val all = eval(velocity, targetX, targetY)
+      val max = all.maxByOrNull { it.y }
+      max
+    }.maxByOrNull { it.y } ?: throw Impossiburu()
+    return "${max.y}"
   }
 
   override suspend fun partTwo(): String {
--- aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D17Test.kt
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D17Test.kt
@@ -11,7 +11,7 @@ internal class Y2021D17Test : BaseDayTest() {
   @Test
   override suspend fun examplePartOne() {
     val input = PuzzleInput.p1Test(Year.Y2021, Day.D17)
-    Y2021D17(input).partOne() shouldBe ""
+    Y2021D17(input).partOne() shouldBe "45"
   }
 
   @Test
@@ -22,7 +22,7 @@ internal class Y2021D17Test : BaseDayTest() {
 
   @Test
   override suspend fun solutionPartOne() {
-    Y2021D17().partOne() shouldBe ""
+    Y2021D17().partOne() shouldBe "9870"
   }
 
   @Test
