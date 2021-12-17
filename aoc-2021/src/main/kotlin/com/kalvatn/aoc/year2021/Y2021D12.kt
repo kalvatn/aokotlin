545bc5477fcf08dc86c8764dd202edb40101f1e8
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2021-12-17 19:48:12 +0100

    aoc-2021-12 prep

---
+++ aoc-2021/src/main/kotlin/com/kalvatn/aoc/year2021/Y2021D12.kt
@@ -0,0 +1,33 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.GenericPuzzle2021
+import com.kalvatn.aoc.core.model.Year
+import com.kalvatn.aoc.core.runner.PuzzleRunner
+import kotlinx.coroutines.runBlocking
+
+class Y2021D12(input: PuzzleInput = PuzzleInput.NULL) : GenericPuzzle2021(Day.D12, input) {
+
+  private val lines by lazy { this.input.lines }
+
+  override suspend fun partOne(): String {
+    lines.forEach {
+      println(it)
+    }
+    return ""
+  }
+
+  override suspend fun partTwo(): String {
+    return ""
+  }
+
+  companion object {
+  }
+}
+
+fun main() = runBlocking {
+  val input = PuzzleInput.p1Test(Year.Y2021, Day.D12)
+  PuzzleRunner.run(Y2021D12(input))
+  PuzzleRunner.run(Y2021D12())
+}
---
+++ aoc-2021/src/test/kotlin/com/kalvatn/aoc/year2021/Y2021D12Test.kt
@@ -0,0 +1,32 @@
+package com.kalvatn.aoc.year2021
+
+import com.kalvatn.aoc.BaseDayTest
+import com.kalvatn.aoc.core.input.PuzzleInput
+import com.kalvatn.aoc.core.model.Day
+import com.kalvatn.aoc.core.model.Year
+import io.kotest.matchers.shouldBe
+
+internal class Y2021D12Test : BaseDayTest() {
+
+  @Test
+  override suspend fun examplePartOne() {
+    val input = PuzzleInput.p1Test(Year.Y2021, Day.D12)
+    Y2021D12(input).partOne() shouldBe ""
+  }
+
+  @Test
+  override suspend fun examplePartTwo() {
+    val input = PuzzleInput.p2Test(Year.Y2021, Day.D12)
+    Y2021D12(input).partTwo() shouldBe ""
+  }
+
+  @Test
+  override suspend fun solutionPartOne() {
+    Y2021D12().partOne() shouldBe ""
+  }
+
+  @Test
+  override suspend fun solutionPartTwo() {
+    Y2021D12().partTwo() shouldBe ""
+  }
+}
